f71f6811792c9e6ad17d28a1a2ccbbbe3711139f
Николай <ibraevnikolay@mail.ru>
2023-02-18 17:10:51 +0500

    Немножко поменяли прогу, добавили else

--- 18/Program.cs
+++ 18/Program.cs
@@ -8,10 +8,10 @@ Console.Write("Введите число от 1 до 4: ");
 int num = int.Parse(Console.ReadLine());
 if(num == 1)
 Console.WriteLine("Диавпазон X > 0, Y > 0");
-if(num == 2)
+else if(num == 2)
 Console.WriteLine("Диавпазон X < 0, Y > 0");
-if(num == 3)
+else if(num == 3)
 Console.WriteLine("Диавпазон X < 0, Y < 0");
-if(num == 4)
+else if(num == 4)
 Console.WriteLine("Диавпазон X > 0, Y < 0");
 
