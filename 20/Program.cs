f21bc25b17934fb052aa61a636454f3679f17c97
Николай <ibraevnikolay@mail.ru>
2023-02-18 22:19:48 +0500

    Сделали правильное решение

--- 20/Program.cs
+++ 20/Program.cs
@@ -6,15 +6,34 @@
 // A(7, -5); B(1, -1)-> 7,21
 //sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
 //double s = Math.Sqrt(...);
+
 Console.Clear();
 Console.Write("Введите координаты Х первой точки: ");
-int X1 = int.Parse(Console.ReadLine());
+double X1 = double.Parse(Console.ReadLine());
 Console.Write("Введите координаты Y первой точки: ");
-int Y1 = int.Parse(Console.ReadLine());
+double Y1 = double.Parse(Console.ReadLine());
 Console.Write("Введите координаты Х второй точки: ");
-int X2 = int.Parse(Console.ReadLine());
+double X2 = double.Parse(Console.ReadLine());
 Console.Write("Введите координаты Y второй точки: ");
-int Y2 = int.Parse(Console.ReadLine());
+double Y2 = double.Parse(Console.ReadLine());
 //double sqrt = ((X1 - Y1)^2 + (Y1 - Y2)^2);
-double result = Math.Sqrt(((X1 - X1)^2 + (Y1 - Y2)^2));
+double result = Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2));//Math.Sqrt - под корень Math.Pow - в квадрат
 Console.WriteLine($"Расстояние между точками {result} ");
+/*
+Решение кирилла
+
+
+Console.Clear();
+Console.Write("Введите X1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+double d1 = Math.Sqrt(-5);
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));//(x1 - x2)* (x1 - x2 )
+
+Console.WriteLine($"d={d:f3}, {d1}");*/
